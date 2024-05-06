77464055a1406096c884e0ed13cac2e682273606
TheRookie <therookiecode@gmail.com>
2024-05-06 09:50:22 -0600

    Completed Reverse String 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	const strArr = string.split("");
+	let reversedStr = [];
+	for (let i = 0; i < strArr.length; i++){
+		reversedStr.push(strArr[strArr.length - 1 - i]);
+	}
+	return reversedStr.join('');
 };
 
+reverseString("hello world");
 // Do not edit below this line
 module.exports = reverseString;
