ad5736b62f6d88113358f51a8a92ba7a610d85b6
TheRookie <therookiecode@gmail.com>
2024-05-07 08:01:30 -0600

    Completed Sum all 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(begin,end) {
+	let sum = 0;
+	const err = "ERROR";
+	if(!Number.isInteger(begin) || !Number.isInteger(end)){
+		return err;
+	}
+	if(begin < 0 || end < 0){
+		return err;
+	}
+	if(end < begin){
+		[begin,end] = [end,begin];
+	}
 
+	for(let i = begin ; i <= end ; i++){
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
