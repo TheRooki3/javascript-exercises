d52ebf9e9d47575b5e549ca774b53089b0821391
TheRookie <therookiecode@gmail.com>
2024-05-07 08:25:16 -0600

    Completed Temp Conversion 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32) * (5/9);
+  if(cel % 1 === 0){
+    return cel;
+  }else{
+    return parseFloat(cel.toFixed(1));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let faren = temp * (9/5) + 32;
+  if(faren % 1 === 0){
+    return faren;
+  }else{
+    return parseFloat(faren.toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
